f754712c400fea275a84323e29ba4ed86f981d29
peytonbrsmith <peytonsmith@gmail.com>
2021-07-12 11:59:55 -0500

    added task 3

---
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,14 @@
+﻿// write a line of text to the console
+
+using System;
+
+namespace _3_writeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
