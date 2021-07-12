5453bbc639e5f8536176cf08de2dc4097481e8aa
peytonbrsmith <peytonsmith@gmail.com>
2021-07-12 14:12:15 -0500

    added task 9

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("percent: {0:0.00%}", percent);
+        Console.WriteLine("currency: {0}", currency.ToString("C"));
+	}
+}
