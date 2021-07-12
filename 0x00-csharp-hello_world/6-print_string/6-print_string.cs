95cec0757eea0248b9cd4341af4d0ba56ba949ae
peytonbrsmith <peytonsmith@gmail.com>
2021-07-12 12:49:16 -0500

    added task 6

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine($"{str}{str}{str}");
+        Console.WriteLine("{0}", str.Substring(0, 9));
+        }
+}
