d2331675cefe31111f73fbe5810f3535bcbab694
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-13 18:59:35 -0500

    added task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        // print a triangle of '\' of length 'length'
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < i + 1; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+        Console.WriteLine();
+    }
+}
