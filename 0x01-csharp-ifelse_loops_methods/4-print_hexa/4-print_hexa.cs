bfd8a987da9b8da0b97b4cee09ead86be1de316e
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-13 17:33:16 -0500

    updated 4

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -9,7 +9,7 @@ namespace _4_print_hexa
             // Write a program that prints all numbers from 0 to 98 in decimal and in hexadecimal (as in the following example)
             for (int i = 0; i < 99; i++)
             {
-                Console.WriteLine("{0,3} = 0x{1,2}", i, i.ToString("X"));
+                Console.Write("{0,3} = 0x{1,2}\n", i, i.ToString("X"));
             }
         }
     }
