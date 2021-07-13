4eddd4b76b7b62d6b983c8b335a11370d4d05d5a
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-13 17:22:30 -0500

    added task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that prints all numbers from 0 to 98 in decimal and in hexadecimal (as in the following example)
+            for (int i = 0; i < 99; i++)
+            {
+                Console.WriteLine("{0,3} = 0x{1,2}", i, i.ToString("X"));
+            }
+        }
+    }
+}
