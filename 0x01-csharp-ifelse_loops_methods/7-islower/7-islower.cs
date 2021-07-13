13b17a8eb0c6d9d6e8b798f0c260bfaf35ea9712
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-13 18:34:04 -0500

    added task 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,15 @@
+﻿class Character : Program
+{
+    public static bool IsLower(char c)
+    {
+        // if c is a lowercase letter, return true
+        if (c >= 'a' && c <= 'z')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
