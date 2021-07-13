c39fb2dbef969ad318fd5768ae6b9bb4a55ff7f8
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-13 16:49:56 -0500

    added task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that prints the alphabet, in lowercase, not followed by a new line.
+            // Print all the letters except q and e
+            // You can only use Console.Write once
+            // You can only use one loop in your code
+            for (int i = 97; i <= 122; i++)
+            {
+                if (i != 113 && i != 101)
+                {
+                    Console.Write((char)i);
+                }
+            }
+        }
+    }
+}
