44193d7f82af7b5d2183f98d0c2e44a94e357a26
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-13 18:04:24 -0500

    updated 5

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -9,10 +9,11 @@ namespace _5_print_comb
             // loop and print integers 0 to 99 seperated by a command and space
             for (int i = 0; i < 100; i++)
             {
-                if (i > 0)
-                    Console.Write(", ");
+                if (i == 99)
+                    Console.Write("{0:D2}\n", i);
                 // write the integer with preceding zeros
-                Console.Write("{0:D2}", i);
+                else
+                    Console.Write("{0:D2}, ", i);
             }
         }
     }
