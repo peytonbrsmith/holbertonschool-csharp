1035ec491bb73a297e92aa1190183b886d580c72
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-13 18:17:01 -0500

    updated task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print all combinations of digits 0 through 9
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i != j && i < j)
+                        Console.Write("{0}{1}, ", i, j);
+                }
+            }
+
+        }
+    }
+}
