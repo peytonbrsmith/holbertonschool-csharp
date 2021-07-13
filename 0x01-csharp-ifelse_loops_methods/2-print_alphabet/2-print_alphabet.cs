6dbd5f8f583cf7fdd9e316158aeba1f897faaa6e
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-13 16:36:39 -0500

    added task 2

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that prints the alphabet lowercase, one write statement
+            for (int i = 97; i < 123; i++)
+            {
+                Console.Write((char)i);
+            }
+        }
+    }
+}
