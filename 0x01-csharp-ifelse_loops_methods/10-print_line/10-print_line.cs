fcd220a6688a7ec582b89fe8ff31a59cad5f0f1b
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-13 18:54:07 -0500

    added task 10

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,13 @@
+
+using System;
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("_");
+        }
+        Console.WriteLine();
+    }
+}
