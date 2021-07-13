f867a7e0d73ed6ce623a85f65a91b0bd92a103bf
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-13 17:54:50 -0500

    added task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // loop and print integers 0 to 99 seperated by a command and space
+            for (int i = 0; i < 100; i++)
+            {
+                if (i > 0)
+                    Console.Write(", ");
+                Console.Write(i);
+            }
+        }
+    }
+}
