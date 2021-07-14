4e0a0b84f560a6e4b1cce1dc6be8aaf7986d1346
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-14 15:31:04 -0500

    added task 7

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -6,7 +6,7 @@ class List {
     {
         if (index < 0 || index >= myList.Count)
         {
-            Console.WriteLine("Index out of range");
+            Console.WriteLine("Index is out of range");
             return myList;
         }
 
