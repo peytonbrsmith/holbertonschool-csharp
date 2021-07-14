41c87894e0434d2547e6c8f28660471b8d89efa2
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-14 16:10:23 -0500

    added task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        // Returns biggest integer
+        return myList.OrderByDescending(x => x.Value).First().Key;
+    }
+}
