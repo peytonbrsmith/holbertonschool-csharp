0ca995f52908c44bde96f436fdab5c33369c3bdc
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-14 16:43:16 -0500

    added task 16

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a jagged array
+            int[][] jaggedArray = new int[3][];
+            jaggedArray[0] = new int[4];
+            jaggedArray[1] = new int[7];
+            jaggedArray[2] = new int[2];
+
+            // loop through the arrays
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                // loop through the array
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    // set the array
+                    jaggedArray[i][j] = j;
+                }
+            }
+            // loop through the arrays
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                // loop through the array
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    // print the array
+                    Console.Write(jaggedArray[i][j]);
+                    if (j != jaggedArray[i].Length - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
