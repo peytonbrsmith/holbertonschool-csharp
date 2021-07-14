d313c396013a902c00fc9dce487fa1a2fed6ab21
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-14 14:05:41 -0500

    added task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        // Create an array of size 'size'
+        int[] arr = new int[size];
+
+        // Fill the array with values
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = i;
+        }
+
+        // Print the array seperated by spaces
+        Console.WriteLine(string.Join(" ", arr));
+
+        return arr;
+    }
+}
+
