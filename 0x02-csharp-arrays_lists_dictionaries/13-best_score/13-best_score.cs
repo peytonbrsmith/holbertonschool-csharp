088f1f83a03f613a9d165dc8e2b6b73d1b1c0c9c
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-14 16:13:09 -0500

    added task 13

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -5,6 +5,11 @@ using System.Linq;
 class Dictionary{
     public static string BestScore(Dictionary<string, int> myList)
     {
+        // if list is empty return "none"
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
         // Returns biggest integer
         return myList.OrderByDescending(x => x.Value).First().Key;
     }
