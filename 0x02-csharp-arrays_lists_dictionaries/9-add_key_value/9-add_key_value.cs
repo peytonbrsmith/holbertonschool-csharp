e29a341f2e1370bba95841bc9c08a1150d38ef08
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-14 15:43:08 -0500

    added task 9

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+class Dictionary{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        
+        if (myDict.ContainsKey(key))
+        {
+            myDict[key] = value;
+        }
+        else
+        {
+            myDict.Add(key, value);
+        }
+        return myDict;
+    }
+}
