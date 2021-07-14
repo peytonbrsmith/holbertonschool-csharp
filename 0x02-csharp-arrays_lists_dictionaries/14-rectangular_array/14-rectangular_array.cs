65ea175365b9600326b1d0daf12331c7c8d74c28
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-14 16:23:06 -0500

    added task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create 5 by 5 array of 0s
+            int[,] array = new int[5, 5];
+
+            // set all indices to 0
+            // for (int row = 0; row < array.GetLength(0); row++)
+            // {
+            //     for (int col = 0; col < array.GetLength(1); col++)
+            //     {
+            //         array[row, col] = 0;
+            //     }
+            // }
+
+            // set index 2,2 to 1
+            array[2, 2] = 1;
+
+            // loop through array and print each index
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int col = 0; col < array.GetLength(1); col++)
+                {
+                    Console.Write(array[row, col] + " ");
+                }
+            Console.WriteLine();
+            }
+        }
+    }
+}
