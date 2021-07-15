98d289c17bffb1bae9186787dbc4c219dc73ba1e
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-15 17:52:43 -0500

    added task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+class MyStack {
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of Items: {aStack.Count}");
+        Console.WriteLine($"Top Item: {aStack.Peek()}");
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        Console.WriteLine($"Stack contains {search}: {aStack.Contains(search)}");
+        if (aStack.Contains(search))
+        {
+            while (aStack.Contains(search))
+            {
+                aStack.Pop();
+            }
+        }
+        if (newItem != null)
+        {
+            aStack.Push(newItem);
+        }
+        return aStack;
+    }
+}
