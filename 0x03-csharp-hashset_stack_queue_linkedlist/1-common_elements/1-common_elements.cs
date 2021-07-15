a2380eb80e657ad6fae01f54ae238ed4049e03ee
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-15 17:28:56 -0500

    added task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        // Create a new sorted list of common elements from list1 and list2
+        List<int> commonElements = new List<int>();
+        foreach (int i in list1)
+        {
+            if (list2.Contains(i))
+            {
+                commonElements.Add(i);
+            }
+        }
+        commonElements.Sort();
+        return commonElements;
+    }
+}
