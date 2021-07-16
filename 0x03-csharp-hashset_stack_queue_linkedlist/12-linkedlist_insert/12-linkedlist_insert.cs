73e043036c5e37805c542335cb2a7e85dd80200f
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-16 00:12:38 -0500

    updated task 12

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -4,30 +4,24 @@ using System.Collections.Generic;
 class LList {
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
-        //Create a new node
-        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
-        //If the list is empty, the new node is the first node
-        if (myLList.First == null)
+        LinkedListNode<int> newNode = null;
+        // insert a node in order of the linked list
+        LinkedListNode<int> current = myLList.First;
+        if (myLList.Count == 0)
         {
-            myLList.AddFirst(newNode);
+            newNode = myLList.AddFirst(n);
         }
-        //If the list is not empty, insert into the sorted linked list
         else
         {
-            LinkedListNode<int> current = myLList.First;
             while (current.Next != null)
             {
-                if (n < current.Next.Value)
+                if (current.Value > n)
                 {
-                    myLList.AddBefore(current, newNode);
                     break;
                 }
                 current = current.Next;
             }
-            if (current.Next == null)
-            {
-                myLList.AddLast(newNode);
-            }
+            newNode = myLList.AddAfter(current, n);
         }
         return newNode;
     }
