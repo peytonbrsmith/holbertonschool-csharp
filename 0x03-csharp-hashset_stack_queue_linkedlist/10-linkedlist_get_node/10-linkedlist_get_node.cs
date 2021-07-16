8d365f82f12ffc99c9bf642f373742e69a03bfaa
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-15 23:39:39 -0500

    added tas k10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+class LList {
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        // Get nth node from head
+        LinkedListNode<int> node = myLList.First;
+        
+        int i;
+        
+        for (i = 0; i < n; i++)
+        {
+            node = node.Next;
+        }
+        if (node.Next == null && i != n)
+        {
+            return 0;
+        }
+        return node.Value;
+    }
+}
