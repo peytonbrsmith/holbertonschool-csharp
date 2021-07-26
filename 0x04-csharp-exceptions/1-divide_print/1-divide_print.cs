9617af03764f56b3d26b0f6bea83417ea7f30b56
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-26 12:23:07 -0500

    added task 1

--- 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -1,1 +1,21 @@
 using System;
+
+class Int {
+    public static void divide(int a, int b)
+    {
+        int result = 0;
+
+        try
+        {
+            result = a / b;
+        }
+        catch (System.DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {result}");
+        }
+    }
+}
