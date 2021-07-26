ba5d5a632e8e8ae756eb2227fe126408e82e745b
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-26 13:24:28 -0500

    added task 3

--- 0x05-csharp-structs_enums/3-dog/3-dog.cs
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -1,1 +1,26 @@
 using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public Dog(string Name, float Age, string Owner, Rating Rate)
+    {
+        name = Name;
+        age = Age;
+        owner = Owner;
+        rating = Rate;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public override string ToString() => $"My name is {name} and I am {age}. I am an {rating} dog. :)";
+}
+
