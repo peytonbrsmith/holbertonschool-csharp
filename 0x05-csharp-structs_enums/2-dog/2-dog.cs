99a8d942a55b4fdb0fad097d0eaebe7af9a6b138
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-26 13:19:31 -0500

    added task 2

--- 0x05-csharp-structs_enums/2-dog/2-dog.cs
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -1,1 +1,24 @@
 using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public Dog(string Name, float Age, string Owner, Rating Rate)
+    {
+        name = Name;
+        age = Age;
+        owner = Owner;
+        rating = Rate;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
+
