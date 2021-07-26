11c4547a3fe9774aed8aea975d7c53cdf4f205fc
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-26 12:13:02 -0500

    added task 0

--- 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -1,1 +1,15 @@
 using System;
+using System.Collections.Generic;
+
+class List {
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int prints = 0;
+        // Print the first n elements of the list
+        for (int i = 0; i < n && i < myList.Count; i++, prints++)
+        {
+            Console.WriteLine(myList[i]);
+        }
+        return prints;
+    }
+}
