a5bb27c0463a22f3dfbb73dd88c322130c36d10d
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-26 12:16:11 -0500

    added task requirement

--- 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -6,9 +6,16 @@ class List {
     {
         int prints = 0;
         // Print the first n elements of the list
-        for (int i = 0; i < n && i < myList.Count; i++, prints++)
+        try
         {
-            Console.WriteLine(myList[i]);
+            for (int i = 0; i < n; i++, prints++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+        }
+        catch (System.ArgumentOutOfRangeException)
+        {
+            ;
         }
         return prints;
     }
