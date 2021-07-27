cc2aa0197a61d95243e435243f67e8e70a296815
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-27 13:54:04 -0500

    added task 1

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -1,1 +1,18 @@
 using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Zombie Class
+    /// </summary>
+    /// <param name="health">health of the zombie</param>
+    public class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
