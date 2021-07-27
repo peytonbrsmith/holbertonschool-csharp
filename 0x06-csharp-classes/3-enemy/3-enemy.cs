06743a65a5f6598295dd853106f9df774b5d8838
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-27 14:29:19 -0500

    added task 3

--- 0x06-csharp-classes/3-enemy/3-enemy.cs
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -1,1 +1,42 @@
 using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Zombie Class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// HEALTH
+        /// </summary>
+        private int health;
+        /// <summary>
+        /// ZOMBIE CONSTRUCTOR
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>
+        /// Constructor with health value set
+        /// </summary>
+        /// <param name="value"></param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+        /// <summary>
+        /// Gets health of zombie
+        /// </summary>
+        /// <returns> health value </returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
