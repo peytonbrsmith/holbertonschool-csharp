3e2145c8cf24e22b90061416f8db8ff47a172fa5
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-27 15:00:16 -0500

    added task 5

--- 0x06-csharp-classes/5-enemy/5-enemy.cs
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -1,1 +1,67 @@
 using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Zombie Class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// HEALTH
+        /// </summary>
+        private int health;
+
+
+        /// <summary>
+        /// ZOMBIE CONSTRUCTOR
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Gets and sets name of zombie
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string name = "(No name)";
+
+        /// <summary>
+        /// Constructor with health value set
+        /// </summary>
+        /// <param name="value"></param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+        
+
+
+        /// <summary>
+        /// Gets health of zombie
+        /// </summary>
+        /// <returns> health value </returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// <summary>
+        /// Overrides string format of zombie
+        /// </summary>
+        /// <returns>formatted string</returns>
+        public override string ToString() {
+            return ($"Zombie Name: {Name} / Total Health: {GetHealth()}");
+        }
+    }
+}
+
