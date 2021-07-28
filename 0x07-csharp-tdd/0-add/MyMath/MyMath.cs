d943aa2931d69ed44dc0255ca43a4639f31dfe33
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-28 18:36:21 -0500

    added base project files

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Class of math operations
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Add function
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns> sum of a and b </returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
