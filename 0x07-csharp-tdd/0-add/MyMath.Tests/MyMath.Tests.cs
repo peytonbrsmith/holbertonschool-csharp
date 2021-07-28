e6edd44a6c07dc89471773a9be14698cad68aa2d
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-28 18:33:42 -0500

    ADDED BASE PROJECT FILES

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using MyMath.Operations
+
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
