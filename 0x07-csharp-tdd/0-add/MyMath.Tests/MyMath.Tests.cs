a01c29bab7688b8eeead62dc755dfa6556ffa7e8
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-28 19:03:54 -0500

    updated unittests

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,20 +1,34 @@
 using NUnit.Framework;
-using MyMath.Operations
+using MyMath;
 
 
-namespace Tests
+namespace MyMath
 {
     public class Tests
     {
+
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public void TestSmallPositives()
         {
-            Assert.Pass();
+            var result = Operations.Add(1, 2);
+            Assert.AreEqual(3, result);
+        }
+        [Test]
+        public void TestSmallNegatives()
+        {
+            var result = Operations.Add(-1, -2);
+            Assert.AreEqual(-3, result);
+        }
+        [TestCase(-62, 300)]
+        public void TestLargeMixedSign(int a, int b)
+        {
+            var result = Operations.Add(a, b);
+            Assert.AreEqual(238, result);
         }
     }
 }
