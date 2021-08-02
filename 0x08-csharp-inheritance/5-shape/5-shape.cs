f494faaf51f860386c21495a8cb3a8697cebedee
Peyton Smith <peytonbrsmith@gmail.com>
2021-08-02 14:37:25 -0500

    added 5

--- 0x08-csharp-inheritance/5-shape/5-shape.cs
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -1,1 +1,16 @@
 using System;
+
+/// <summary>
+/// shape class
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// area func
+    /// </summary>
+    /// <returns>nothing</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
