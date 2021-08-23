aefccf8e22384842d7023f050a0fc1856aff5eb1
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-23 18:19:25 -0500

    added 6

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,1 +1,13 @@
 using System;
+
+class VectorMath {
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double[] result = new double[vector1.Length];
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            result[i] = vector1[i] + vector2[i];
+        }
+        return result;
+    }
+}
