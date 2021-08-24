3083b4bd50bc3a40ece8cb51c1c4ff5e2dd662f4
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-24 14:18:58 -0500

    added 16

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -1,1 +1,26 @@
 using System;
+
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+
+        if ((matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3) || (matrix.GetLength(1) != 2 && matrix.GetLength(1) != 3))
+        {
+            double[,] err = new double[,] { { -1 } };
+            err[0, 0] = -1;
+            return err;
+        }
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                result[i, j] = matrix[i, j] * scalar;
+            }
+        }
+
+        return result;
+    }
+}
