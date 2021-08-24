fd952f639cc35ce5cdec9161a136161a7041d1f1
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-24 15:04:00 -0500

    updated 18

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -22,7 +22,7 @@ class MatrixMath {
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
 
     {
-        double[,] result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+        double[,] result = new double[matrix2.GetLength(0), matrix2.GetLength(1)];
 
         for (int i = 0; i < matrix1.GetLength(0); i++)
         {
