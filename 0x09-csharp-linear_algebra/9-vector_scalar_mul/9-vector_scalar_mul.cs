248f71dc741fac0ac2755b35560c02c0bb24423c
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-24 12:45:57 -0500

    added 9

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -1,1 +1,20 @@
 using System;
+
+class VectorMath {
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] result = new double[vector.Length];
+
+        if ((vector.Length == 2 || vector.Length == 3))
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                result[i] = vector[i] * scalar;
+            }
+            return result;
+        }
+        double[] err = new double[1];
+        err[0] = -1;
+        return err;
+    }
+}
