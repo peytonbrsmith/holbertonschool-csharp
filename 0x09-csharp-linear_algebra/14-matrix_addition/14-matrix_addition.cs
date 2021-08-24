d675e71d15ae45b12f5ecdfbe970c5c734b82eaf
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-24 14:05:35 -0500

    added 14

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,20 +1,19 @@
 using System;
+using System.Linq;
 
 class MatrixMath
 {
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        double[,] result;
+        double[,] result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
 
-        // check the size of the matrices
-        if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+        if (matrix1.Length != matrix2.Length || matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
         {
-            // return matrix with -1
-            result = new double[1, 1];
-            result[0, 0] = -1;
-            return result;
+            double[,] err = new double[,] { { -1 } };
+            err[0, 0] = -1;
+            return err;
         }
-        result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+
         for (int i = 0; i < matrix1.GetLength(0); i++)
         {
             for (int j = 0; j < matrix1.GetLength(1); j++)
@@ -22,6 +21,7 @@ class MatrixMath
                 result[i, j] = matrix1[i, j] + matrix2[i, j];
             }
         }
+
         return result;
     }
 }
