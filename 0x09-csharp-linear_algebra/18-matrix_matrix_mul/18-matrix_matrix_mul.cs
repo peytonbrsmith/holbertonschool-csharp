fd7b67c0646b5c120eaebf413b0a3301db178d04
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-24 15:12:34 -0500

    updated 18

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -24,7 +24,7 @@ class MatrixMath {
     {
         double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
 
-        if (matrix1.GetLength(1) != matrix2.GetLength(0) || matrix1.GetLength(0) != matrix2.GetLength(1))
+        if (matrix1.GetLength(0) != matrix2.GetLength(1) || matrix1.GetLength(1) != matrix2.GetLength(0))
         {
             double[,] err = new double[,] { { -1 } };
             err[0, 0] = -1;
