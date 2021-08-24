5f3ee229ee92bb526e15427d51a330f83bfb7606
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-24 13:30:32 -0500

    added 14

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -10,11 +10,11 @@ class MatrixMath
         if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
         {
             // return matrix with -1
-            double[,] result = new double[1, 1];
+            result = new double[1, 1];
             result[0, 0] = -1;
             return result;
         }
-        double[,] result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+        result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
         for (int i = 0; i < matrix1.GetLength(0); i++)
         {
             for (int j = 0; j < matrix1.GetLength(1); j++)
