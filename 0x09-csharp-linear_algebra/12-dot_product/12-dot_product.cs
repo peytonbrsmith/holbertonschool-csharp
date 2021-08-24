93dc16c84efd8833068130fef1cd02fc41faa175
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-24 13:08:11 -0500

    added 12

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -1,1 +1,19 @@
 using System;
+
+class VectorMath {
+    public static double DotProduct(double[] vector1, double[] vector2)
+
+    {
+        double result = 0;
+
+        if (vector1.Length == vector2.Length && (vector1.Length == 2 || vector1.Length == 3))
+        {
+            for (int i = 0; i < vector1.Length; i++)
+            {
+                result += vector1[i] * vector2[i];
+            }
+            return result;
+        }
+        return -1;
+    }
+}
