b35d158be98fc30204591094c151f7ccebe6ec8a
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-25 17:41:19 -0500

    added 28

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -1,1 +1,21 @@
 using System;
+
+class MatrixMath{
+    public static double Determinant(double[,] matrix)
+    {
+        if(matrix.GetLength(1) == 2)
+        {
+            return (matrix[0,0] * matrix[1,1]) - (matrix[0,1] * matrix[1,0]);
+        }
+        else if (matrix.GetLength(1) == 3)
+        {
+            double  det = 0;
+
+            for(int i=0;i<3;i++)
+                det = det + (matrix[0,i]*(matrix[1,(i+1)%3]*matrix[2,(i+2)%3] - matrix[1,(i+2)%3]*matrix[2,(i+1)%3]));
+            return det;
+        }
+
+        return -1;
+    }
+}
