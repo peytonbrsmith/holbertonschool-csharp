79afc3de052ea0c2b8728f7f269d866ea7fd7ac8
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-25 17:43:10 -0500

    added 28

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -5,7 +5,7 @@ class MatrixMath{
     {
         if(matrix.GetLength(1) == 2)
         {
-            return (matrix[0,0] * matrix[1,1]) - (matrix[0,1] * matrix[1,0]);
+            return Math.Round((matrix[0,0] * matrix[1,1]) - (matrix[0,1] * matrix[1,0]), 2);
         }
         else if (matrix.GetLength(1) == 3)
         {
@@ -13,7 +13,7 @@ class MatrixMath{
 
             for(int i=0;i<3;i++)
                 det = det + (matrix[0,i]*(matrix[1,(i+1)%3]*matrix[2,(i+2)%3] - matrix[1,(i+2)%3]*matrix[2,(i+1)%3]));
-            return det;
+            return Math.Round(det, 2);
         }
 
         return -1;
