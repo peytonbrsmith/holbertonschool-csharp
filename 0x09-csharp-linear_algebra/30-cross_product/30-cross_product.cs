c3f73ce2fa67c828e35ed0474fb6fac3625ef465
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-25 18:21:07 -0500

    added 30

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -1,1 +1,21 @@
 using System;
+
+class VectorMath
+{
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != 3 || vector2.Length != 3)
+        {
+            double[,] error = new double[1, 1];
+            error[0, 0] = -1;
+            return error;
+        }
+
+        double[] result = new double[3];
+
+        result[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1];
+        result[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2];
+        result[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+        return result;
+    }
+}
