b0620508b2694dcd39fa7ce48c835b815f3a10ca
peytonbrsmith <peytonbrsmith@gmail.com>
2021-10-25 13:26:17 -0500

    added task 2

---
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,62 @@
+using System;
+
+class Queue<T>
+{
+
+    Node head;
+    Node tail;
+    int count;
+    public System.Type CheckType()
+    {
+       return (typeof(T));
+    }
+
+    public class Node
+    {
+        public object value = null;
+        public Node next = null;
+
+        public object Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+    }
+
+    public void Enqueue(T value)
+    {
+        Node node = new Node();
+        node.Value = value;
+
+        if (head == null)
+        {
+            head = node;
+            tail = node;
+        }
+        else
+        {
+            tail.next = node;
+            tail = node;
+        }
+
+        count++;
+    }
+
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        T Val = (T) head.Value;
+        head = head.next;
+        count--;
+        return Val;
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+}
