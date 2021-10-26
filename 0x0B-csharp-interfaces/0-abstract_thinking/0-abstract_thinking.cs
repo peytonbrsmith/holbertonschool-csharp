b28cc51434f77eb4cbc6e14612b9225a6ad8ffe0
peytonbrsmith <peytonbrsmith@gmail.com>
2021-10-26 14:01:16 -0500

    added xml documentation

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,9 +1,18 @@
 using System;
-
+/// <summary>
+/// Base class
+/// </summary>
 public abstract class Base
 {
+    /// <summary>
+    /// Name of instance of base or inherited class
+    /// </summary>
     public string name;
 
+    /// <summary>
+    /// Overrides string method withen required format
+    /// </summary>
+    /// <returns> String </returns>
     public override string ToString()
     {
         return (this.name + " is a " + this.GetType().Name);
