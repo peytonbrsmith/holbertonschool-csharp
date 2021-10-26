0486620ce7792af16dd48e080caa7472fac04246
peytonbrsmith <peytonbrsmith@gmail.com>
2021-10-26 14:43:11 -0500

    added task 1

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,1 +1,46 @@
 using System;
+
+abstract class Base
+{
+    public string name;
+
+    public override string ToString()
+    {
+        return (this.name + " is a " + this.GetType().Name);
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+    {
+    }
+
+    public void Break()
+    {
+    }
+
+    public void Collect()
+    {
+    }
+}
