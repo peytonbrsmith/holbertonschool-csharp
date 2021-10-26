43871254752ece744e2e3a560d9baa2e0d079364
peytonbrsmith <peytonbrsmith@gmail.com>
2021-10-26 14:59:05 -0500

    added 2

--- 0x0B-csharp-interfaces/2-doors/2-doors.cs
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -1,1 +1,52 @@
 using System;
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return (this.name + " is a " + this.GetType().Name);
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class Door : Base, IInteractive
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the {0}. It's locked.", this.name);
+    }
+
+    public void Break()
+    {
+    }
+
+    public void Collect()
+    {
+    }
+}
